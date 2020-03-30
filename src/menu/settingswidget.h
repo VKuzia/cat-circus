0282ca1057939b729656943e96fcb401d6533601
Vladimir Kuzmitski <v.kuzia.mail@gmail.com>
2020-03-30 10:47:04 +0300

    Add menu core

---
+++ src/menu/settingswidget.h
@@ -0,0 +1,27 @@
+#ifndef SETTINGSWIDGET_H
+#define SETTINGSWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class SettingsWidget;
+}
+
+class SettingsWidget : public QWidget {
+  Q_OBJECT
+
+ public:
+  explicit SettingsWidget(QWidget* parent = nullptr);
+  ~SettingsWidget();
+
+ signals:
+  void MainMenu();
+
+ public slots:
+  void ReturnToMainMenu();
+
+ private:
+  Ui::SettingsWidget* ui;
+};
+
+#endif  // SETTINGSWIDGET_H
