fc9c4aaebf4b422757a3f74c2ed597e950879fb0
Artiom <you@example.com>
2020-04-08 00:51:23 +0300

    Add physics

--- src/game/physics/physics.cpp
+++ src/game/physics/physics.cpp
@@ -5,9 +5,9 @@
 
 
 
-QPair<int,int> physics::newPosition(QPair<int, int>* current_c,
+QPair<int, int> physics::newPosition(QPair<int, int>* current_c,
                                      const QVector2D& vilocity) {
-    QPair<int,int> answer = {0,0};
+    QPair<int, int> answer = {0,0};
     int new_x = current_c->first + vilocity.x();
     int new_y = current_c->second+vilocity.y();
     answer = {new_x, new_y};
@@ -20,7 +20,7 @@ QVector2D physics::ellasticCollisionWall(const QVector2D& impulse,
     QVector2D answer{0, 0};
     QVector2D wall_vector{static_cast<float>(cos(wall_angle)),
                           static_cast<float>(sin(wall_angle))};
-    QPair<double,double> normal_wall{-wall_vector.y(), wall_vector.x()};
+    QPair<double, double> normal_wall{-wall_vector.y(), wall_vector.x()};
     double alpha_cos = QVector2D::dotProduct(impulse, wall_vector)/
                        (impulse.length()*wall_vector.length());
     double alpha_sin = sin(acos(alpha_cos) + wall_angle);
@@ -31,7 +31,8 @@ QVector2D physics::ellasticCollisionWall(const QVector2D& impulse,
 }
 
 
-QPair<int, int> physics::Advance(QVector2D* vilocity,const QPair<int,int> coordinates) {
+QPair<int, int> physics::Advance(QVector2D* vilocity,
+                                 const QPair<int, int> coordinates) {
     QPair<int, int> answer = {coordinates.first + vilocity->x(),
                               coordinates.second + vilocity->y() + g.y()/2};
     vilocity->setY(vilocity->y() - g.y());
@@ -57,5 +58,4 @@ void ellasticColisionTwoObjects(const double& mass_f, QVector2D* vilocity_f,
     vilocity_f->setY(u1.y());
     vilocity_s->setX(u2.x());
     vilocity_s->setY(u2.y());
-
 }
