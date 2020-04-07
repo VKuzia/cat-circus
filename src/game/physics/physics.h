f4934630deb4db904e7c6f69590e5cf014bbd806
Artiom <you@example.com>
2020-04-08 00:42:11 +0300

    Fix travis

--- src/game/physics/physics.h
+++ src/game/physics/physics.h
@@ -6,26 +6,23 @@
 #include <QVector2D>
 
 
-class physics
-{
+class physics {
+
 public:
-
-
     static void ellasticColisionTwoObjects(const double&, QVector2D*,
                                            const double&, QVector2D*);
 
     static QVector2D ellasticCollisionWall(const QVector2D&,
-                                                      double);
+                                                      double  );
 
-    static QPair<int,int> Advance(QVector2D*,const QPair<int,int>);
+    static QPair<int,int> Advance(QVector2D*,const QPair<int, int>);
 
 
 private:
-
     constexpr static const QVector2D g =  {0, 9.80655};
 
     static QPair<int,int> newPosition(QPair<int, int>*,
                                       const QVector2D&);
 };
 
-#endif // PHYSICS_H
+#endif  // PHYSICS_H
