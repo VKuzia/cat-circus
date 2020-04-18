b50643a4315ab568ea4bfd46e400bfa6115df0fd
Artiom <you@example.com>
2020-04-18 16:44:22 +0300

    Add physics

--- src/game/physics/physics.h
+++ src/game/physics/physics.h
@@ -8,17 +8,17 @@
 
 
 namespace   Physics {
-    static void CollideTwoObjects(const float& mass1,
-                                  QVector2D* velocity1,
-                                  const float& mass2,
-                                  QVector2D* velocity2);
+    void CollideTwoObjects(const float& mass1,
+                           QVector2D* velocity1,
+                           const float& mass2,
+                           QVector2D* velocity2);
 
 
-    static QVector2D Reflect(const QVector2D& impulse,
-                             double wall_angle);
+    QVector2D Reflect(const QVector2D& impulse,
+                      double wall_angle);
 
-    static QPoint Advance(QVector2D* velocity,
-                          const QPoint& coordinates);
+    QPoint Advance(QVector2D* velocity,
+                   const QPoint& coordinates);
 
 
     extern const QVector2D kGravity;
