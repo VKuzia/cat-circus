7e059db7248eb1f3933186ebe23ac5bc0906b305
Artiom <you@example.com>
2020-04-21 12:05:40 +0300

    add physics

--- src/game/physics/physics.cpp
+++ src/game/physics/physics.cpp
@@ -2,9 +2,9 @@
 
 #include <QVector2D>
 
-const QVector2D Physics::kGravity = {0, 9.80655};
+const QVector2D physics::kGravity = {0, 9.80655};
 
-void Physics::CollideTwoObjects(const float &mass1, QVector2D *velocity1,
+void physics::CollideTwoObjects(const float &mass1, QVector2D *velocity1,
                                 const float &mass2, QVector2D *velocity2) {
   QVector2D velocity = *velocity1 - *velocity2;
   QVector2D u1 = velocity * mass2 / (mass1 + mass2);
@@ -20,7 +20,7 @@ void Physics::CollideTwoObjects(const float &mass1, QVector2D *velocity1,
       static_cast<float>(u2.y() * cos(angle_s) - u2.x() * sin(angle_s))};
 }
 
-QVector2D Physics::Reflect(const QVector2D &impulse, double wall_angle) {
+QVector2D physics::Reflect(const QVector2D &impulse, double wall_angle) {
   QVector2D wall_vector{static_cast<float>(cos(wall_angle)),
                         static_cast<float>(sin(wall_angle))};
   QPair<float, float> normal_wall{-wall_vector.y(), wall_vector.x()};
@@ -31,7 +31,7 @@ QVector2D Physics::Reflect(const QVector2D &impulse, double wall_angle) {
   return {(impulse.x() * alpha_cos), (impulse.y() * alpha_sin)};
 }
 
-QPoint Physics::Advance(QVector2D *velocity, const QPoint &coordinates) {
+QPoint physics::Advance(QVector2D *velocity, const QPoint &coordinates) {
   QPoint answer = {
       static_cast<int>(coordinates.x() + velocity->x()),
       static_cast<int>(coordinates.y() + velocity->y() + kGravity.y() / 2)};
@@ -39,10 +39,10 @@ QPoint Physics::Advance(QVector2D *velocity, const QPoint &coordinates) {
   return answer;
 }
 
-QVector2D Throw(QPointF start, QPointF finish, float time) {
+QVector2D physics::Throw(QPointF start, QPointF finish, float time) {
   QVector2D answer;
   answer.setX((finish.x() - start.x()) / time);
   answer.setY((finish.y() - start.y()) / time +
-              (Physics::kGravity.y() * time) / 2);
+              (physics::kGravity.y() * time) / 2);
   return answer;
 }
--- src/game/physics/physics.h
+++ src/game/physics/physics.h
@@ -5,7 +5,7 @@
 #include <QVector2D>
 #include <QtMath>
 
-namespace Physics {
+namespace physics {
 void CollideTwoObjects(const float &mass1, QVector2D *velocity1,
                        const float &mass2, QVector2D *velocity2);
 
@@ -17,7 +17,7 @@ QVector2D Throw(QPointF start, QPointF finish, float time);
 
 extern const QVector2D kGravity;
 // clang-format off
-};  // namespace Physics
+};  // namespace physics
 // clang-format on
 
 #endif // PHYSICS_H
