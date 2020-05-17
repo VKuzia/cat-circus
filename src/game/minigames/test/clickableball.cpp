9b7e23b99d377c6180d31b3a4da19a7835b0fc36
Artiom <artiomstasiukevich@gmail.com>
2020-05-18 00:30:24 +0300

    Delete comment

--- src/game/minigames/test/clickableball.cpp
+++ src/game/minigames/test/clickableball.cpp
@@ -8,16 +8,7 @@ ClickableBall::ClickableBall(GameView* game_view, qreal width, qreal height,
                              QPointF pos)
     : GameObject(game_view, width, height, pos) {}
 
-void ClickableBall::SetUp() {
-  //  qreal width = boundingRect().width();
-  //  qreal height = boundingRect().height();
-  //  QPixmap pixmap(boundingRect().size().toSize());
-  //  QPainter painter(&pixmap);
-  //  painter.setBrush(kBasicColor);
-  //  // Some edge pixels left to render ball without edges cut
-  //  painter.drawEllipse(1, 1, qFloor(width) - 2, qFloor(height) - 2);
-  setPixmap(LoadPixmap("plate/sausage.png"));
-}
+void ClickableBall::SetUp() { setPixmap(LoadPixmap("plate/sausage.png")); }
 
 void ClickableBall::mousePressEvent(QGraphicsSceneMouseEvent*) {
   emit Clicked();
